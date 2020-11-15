bbac6b408bcdd9e617655cfb3704a4a43c72df0f
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-11-15 17:02:37 +0900

    sprintf関数

--- section02/sample12.php
+++ section02/sample12.php
@@ -19,8 +19,10 @@
 <h2>Practice</h2>
 <pre>
 <?php
-$date = sprintf('%04d年 % 2d月 % 2d日 %s', 2018, 12, 5, '金');
-print($date);
+// %の次に置換する文字を書く。今回は 0 で置き換える
+// 文字を表示したいときは %04dを %04sにする。 s = string
+$date = sprintf('%04d年 %02d月 %02d日 %s ', 2018, 1, 3, '日曜日');
+printf($date);
 ?>
 </pre>
 </main>

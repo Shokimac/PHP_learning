7a5ef0865bf2a971a36908da9c2f2b842fbf4ffc
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-11-15 13:26:54 +0900

    for文

--- section02/sample07.php
+++ section02/sample07.php
@@ -19,11 +19,9 @@
 <h2>Practice</h2>
 <pre>
 <?php
-for ($i=1; $i<=1000; $i++):
-    $date = strtotime('+' . $i . 'day');
-    print (date('n/j(D)', $date));
-    print "\n";    
-endfor;
+for($i = 1; $i <= 365; $i++){
+    print($i . "\n");
+}
 ?>
 </pre>
 </main>

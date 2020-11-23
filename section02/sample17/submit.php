3b715e3ac11b86d0f4d9800df1d6a54375b195d9
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-11-23 21:31:35 +0900

    フォームからの入力値を、PHP側で受け取る

--- section02/sample17/submit.php
+++ section02/sample17/submit.php
@@ -18,7 +18,14 @@
 <main>
 <h2>Practice</h2>
 <pre>
-    お名前： <?php print(htmlspecialchars($_REQUEST['my_name'], ENT_QUOTES)); ?>
+    <!-- $_REQUESTはグローバル変数で、inputタグのname属性を指定することでフォームの値を受け取れる -->
+    <!-- htmlspecialchars は、フォームに入力された値にHTMLタグがあった場合にそれを逃す(エスケープする)役割を持つ。（セキュリティの観点） -->
+    <!-- ENT_QUOTESは、定義済み定数である。 -->
+    <!-- $_REQUEST は、$_GET も $_POSTも両方取得することができる。
+    が、セキュリティ観点上パスワードなどの機密情報がGETで送られてきた場合も受け取れてしまう為、
+    メソッドが分かっている場合には、$_GET や $_POSTで使い分けることが重要。-->
+    <!-- HTTPメソッドのGETは、URLに載せてデータを送信。POSTは裏側で送信できる。 フォーム側とPHP側で合わせる必要がある。 -->
+    お名前：<?php print(htmlspecialchars($_REQUEST['my_name'], ENT_QUOTES)); ?>
 </pre>
 </main>
 </body>    

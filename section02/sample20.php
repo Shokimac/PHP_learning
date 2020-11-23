8bce4459677e6a1bb495ea57ac246fd9feb8c967
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-11-23 22:17:06 +0900

    全角数字を半角数字に直して、条件判定させる

--- section02/sample20.php
+++ section02/sample20.php
@@ -19,13 +19,16 @@
 <h2>Practice</h2>
 <pre>
     <?php
-    $age = '20';
+    $age = '２０';
 
+    // mbはマルチバイトの略で全角を指す。つまり、様々な仮名をコンバートする。引数に、$age, 'n', と指定すると半角数字に変換するという動きになる。
     $age = mb_convert_kana($age, 'n', 'UTF-8');
+
+    // is_numericは数値であるかを判定する
     if (is_numeric($age)) {
         print($age . '歳');
     } else {
-        print('※ 年齢が数字ではありません');
+        print('年齢が数字ではありません');
     }
     ?>
 </pre>
